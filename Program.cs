ee2b692d0cf61969c7a14c2339c31bd9b9cb23ef
Konashina <Konashina54@gmail.com>
2025-11-20 15:36:36 +0300

    Task 4: Added group number and Full name

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine($"Текущая дата: {DateTime.Now.ToShortDateString()}");
+Console.WriteLine("Группа: IS23A");
+Console.WriteLine("ФИО: Konashina D.S.");
